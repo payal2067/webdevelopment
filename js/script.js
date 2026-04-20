c5ddd8f8652ba5f748c3962ced1cc840f184d950
payal2067 <payalsanodiya89@gmail.com>
2026-04-21 01:21:16 +0530

    Add registration, restaurant dashboard, sitemap, and terms of service pages with responsive design and navigation

---
+++ js/script.js
@@ -0,0 +1,20 @@
+ console.log("hello");
+
+ var a = 5;
+ var a = "hello";
+
+ let c = "payal";
+let d = 4;
+c = 5;
+let b = true;
+const pi = 3.14; // Use constant vale
+
+let x = 10;
+let y = '10';
+console.log(x==y);
+
+console.log(x===y);
+
+
+
+
