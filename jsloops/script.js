ce96b6cbc5922d50a82c08f9959db7a0fc9f09c6
payal2067 <payalsanodiya89@gmail.com>
2026-04-29 23:00:20 +0530

    Refactor heading in index.html and add new jsloops directory with initial script.js file

---
+++ jsloops/script.js
@@ -0,0 +1,26 @@
+//  for(var i=1; i<=10; i++ ){
+//     console.log(2*i);
+//     console.log(i,i*i*i)
+//  }
+// let a = [24, 58, 33, 86, 96, 12, 53, 23, 32, 100]
+// for(var i=0; i<10; i++){
+//     console.log(a[i])
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log(a[i]);
+//     i++
+// }
+
+//  var i=1;
+//  while(i<=10){
+// console.log(2*i);
+// i++
+//  }
+
+ var i=1;
+ do{
+    console.log(2*i);
+    i++
+ }while(i<+10);
