9e0a8e8a181764bc4079d44cdb5756e54b998e1d
payal2067 <payalsanodiya89@gmail.com>
2026-04-30 22:39:11 +0530

    Add initial HTML and JavaScript files for conditional statements, for loops, and while loops with examples

---
+++ javascript/jsloops/whileLoop/script.js
@@ -0,0 +1,97 @@
+//  1.Write a program to print numbers from 1 to 5 using a while loop.
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// 2.Write a program to print numbers from 5 to 1 using a while loop.
+let i = 5;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
+
+// 3.Write a program to print numbers starting from 2 and keep adding 2 while the number becomes less than 11.
+let i = 2;
+
+while (i < 11) {
+    console.log(i);
+    i = i + 2;
+}
+
+// 4.Write a program to calculate the sum of numbers from 10 to 5 using a while loop.
+let i = 10;
+let sum = 0;
+
+while (i >= 5) {
+    sum = sum + i;
+    i--;
+}
+
+console.log("Sum =", sum);
+
+// 5.Create a while loop and explain the role of initialization, condition, and increment in it.
+let i = 1; // Initialization
+
+while (i <= 5) { // Condition
+    console.log(i); // Work
+    i++; // Increment / Update
+}
+
+// 6.Write a loop and trace its execution step-bystep (initialization → condition → work → update → repeat).
+let i = 1;
+
+while (i <= 3) {
+    console.log(i);
+    i++;
+}
+
+// 7.Create a loop where the condition is false at the beginning and explain what happens.
+let i = 10;
+
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// 8.Write a loop and explain what happens if the variable is not updated inside the loop.
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+// }
+
+// 9.Create a loop and explain when and why it becomes an infinite loop.
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     // i++ missing ❌
+// }
+
+// 10. Write a loop and explain how the condition controls the stopping of the loop.
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// 11. Create a loop and explain how changing increment to decrement affects the execution.
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i--; // galat direction
+}
+
+// 12. Write a loop and explain how many times it will run based on the condition.
+let i = 5;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
