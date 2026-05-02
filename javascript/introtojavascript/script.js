69fff4a8f7165f1c1a97d1314c7a12bf96c85031
payal2067 <payalsanodiya89@gmail.com>
2026-05-02 11:18:03 +0530

    Add initial HTML, CSS, and JavaScript files for introduction to JavaScript

---
+++ javascript/introtojavascript/script.js
@@ -0,0 +1,12 @@
+let x = 4;
+let y = 3;
+console.log(x + y);
+
+console.log("Hello World");
+
+let name = "Payal";
+console.log(name);
+
+function showmessage() {
+  alert("Button was clicked!");
+}
