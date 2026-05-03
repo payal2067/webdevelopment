94cfc026b61332f5e899445fc339ee105716bce8
payal2067 <payalsanodiya89@gmail.com>
2026-05-03 23:24:31 +0530

    Add initial HTML and JavaScript files for exercises on variable declaration and operations

---
+++ javascript/Exercises-1/script.js
@@ -0,0 +1,120 @@
+//  Exercise 1
+// Connect a JavaScript file to HTML using <script> tag.
+
+// Exercise 2
+// Declare variables using:
+// var
+// let
+// const
+var a = 25;                     
+let b = 13;
+const studentId = 104;          // its fixed 
+
+// Exercise 3
+// Create variables:
+// name = "Amit"
+// age = 22
+// Print them.
+let name = "Payal";
+let age = 24;
+console.log("name:" + name , ", age" + age);
+
+// Exercise 4
+// Declare a variable first, assign value later.
+// Example: create variable, then assign 10.
+let c ;
+c = 10;
+console.log(c);
+
+// Exercise 5
+// Create valid variable names.
+// Example: userName, _age
+// Try invalid names.
+// Example: 123name (not allowed)
+let userName = "Payal";
+let _age = 24;
+console.log("userNAme:" + userName , ", _age:" + age);
+// let 123name = "Payal";
+
+
+// Exercise 6
+// Create:
+// number → 10
+// string → "Hello"
+// boolean → true
+
+
+// Exercise 7
+// Check type of values.
+// Example: type of "Hello" → string
+
+// Exercise 8
+// Create:
+// undefined variable
+// null variable
+// Example:
+// undefined → declared but no value
+// null → intentionally empty
+
+// Exercise 9
+// Create variable with number value.
+// Change it to string.
+// Example: 10 → "Hello"
+
+// Exercise 10
+// Perform:
+// addition → 10 + 5
+// subtraction → 10 - 5
+// multiplication → 10 * 5
+// division → 10 / 5
+
+// Exercise 11
+// Find remainder.
+// Example: 10 % 3 → 1
+
+// Exercise 12
+// Use:
+// x = 10
+// x += 5
+// x -= 2
+
+// Exercise 13
+// Compare values:
+// Example:
+// 5 == "5" → true
+// 5 === "5" → false
+
+// Exercise 14
+// Use:
+// greater than → 10 > 5
+// less than → 5 < 10
+
+// Exercise 15
+// Use AND operator.
+// Example: age > 18 AND hasID = true
+
+// Exercise 16
+// Use NOT operator.
+// Example: !true → false
+
+// Exercise 17
+// Use increment.
+// Example: 5 becomes 6
+
+// Exercise 18
+// Check difference:
+// Example:
+// x++ → use first, then increase
+// ++x → increase first
+
+// Exercise 19
+// Check condition using ternary.
+// Example:
+// age ≥ 18 → "Adult"
+// else → "Minor"
+
+// Exercise 20
+// Check types using typeof.
+// Example:
+// 10 → number
+// "Hi" → string
