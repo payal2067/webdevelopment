70d22f52b1d0f5ce045f6efaaaece8f8766c9ebd
payal2067 <payalsanodiya89@gmail.com>
2026-05-09 19:39:22 +0530

    Add HTML, CSS, and JavaScript files for age calculator functionality

---
+++ javascript/agecalculator/script.js
@@ -0,0 +1,9 @@
+ document.querySelector("form").addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    const dob = document.getElementById("DOB").value;
+    const curDate = document.getElementById("CurrentDate").value;
+
+    const Age = Number(cur) 
+
+ })
