72ee05740505d9c8b814bcd14339e7f594e5c221
payal2067 <payalsanodiya89@gmail.com>
2026-06-07 22:47:57 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/MYFIRSTREACTAPP/src/componants/Header.jsx
+++ frontend/MYFIRSTREACTAPP/src/componants/Header.jsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom";
-
+import { Form, Link } from "react-router-dom";
+import logoLight from "../assets/logo-light.png";
 function Header() {
   return (
     <>
       <div className="header p-2 d-flex justify-content-between align-items-center">
-        <div className=" fs-4 fw-bold">My Company</div>
-
+        <div className=" fs-4 fw-bold">
+          <Link to={"/home"}>
+            <img src={logolight} alt="" />
+          </Link>
+        </div>
+        // import contactImage from "./assets/contactPage.jpg";
+        {/*  <img src={contactImage} alt="" className="w-100" /> */}
         <div className="d-flex gap-4">
           <Link to={"/"}>Home</Link>
           <Link to={"/about"}>About</Link>
           <Link to={"/product"}>Product</Link>
           <Link to={"/contact-us"}>Contact Us</Link>
         </div>
-
         <div className="d-flex gap-3">
           <Link to={"/login"}>
             <button className="btn btn-Login btn-outline-primary">Login</button>
